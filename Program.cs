93e35db9de755a22cf0bb0da722385a9e610ea8d
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-18 21:23:38 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_for01
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//倒星形三角形
+			//*****
+			//****
+			//***
+			//**
+			//*
+
+			for (int i=5;i>0;i--)
+			{
+				string row = new string('*', i);
+				Console.WriteLine(row);
+			}
+			Console.ReadKey();
+		}
+	}
+}
